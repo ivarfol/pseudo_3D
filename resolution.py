50c9a862cb311f2176c06d2717ef42c232d683d7
iaroslav <varfolomeeviaroslav@gmail.com>
2025-01-05 20:30:47 +0000

    add program for calculating variables for different resolutions

---
+++ resolution.py
@@ -0,0 +1,26 @@
+while True:
+    try:
+        h = int(input("desired vertical resolution (must be divizible by 2): "))
+    except:
+        print("must be an integer!")
+        continue
+    if h % 2 != 0:
+        print("must be divizible by two")
+    else:
+        break
+while True:
+    try:
+        length = int(input("desired hirizontal resolution: "))
+        break
+    except:
+        print("must be an integer!")
+while True:
+    try:
+        fov = float(input("desired fov in pi radians: "))
+        break
+    except:
+        print("must be a number!")
+print("h =", h)
+print("length =", length)
+print("step =", fov / length)
+print("shift =", -fov / 2)
